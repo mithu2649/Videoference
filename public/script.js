5094483a6d1993a7ae02c26435d51316e7ba3f9a
Mithu Mondal <mithu2649@gmail.com>
2021-08-02 22:30:35 +0530

    --no-commit-message

--- public/script.js
+++ public/script.js
@@ -7,6 +7,7 @@ const myPeer = new Peer(undefined, {
 });
 
 const myVideo = document.createElement('video');
+myVideo.setAttribute('id', 'myVideo');
 myVideo.muted = true;
 const peers = {};
 navigator.mediaDevices.getUserMedia({
