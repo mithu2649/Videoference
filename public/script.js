2d2186405c2848ea364fdfa496af58087e761757
Mithu Mondal <mithu2649@gmail.com>
2021-08-02 21:50:36 +0530

    --no-commit-message

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/');
 const videoGrid = document.querySelector('#video-grid');
 const myPeer = new Peer(undefined, {
-    host: 'https://peerjs-serve.herokuapp.com',
+    host: 'peerjs-serve.herokuapp.com',
     port: '443',
     secure: true
 });
